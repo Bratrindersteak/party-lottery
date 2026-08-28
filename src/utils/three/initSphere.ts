97c9ac94cbd447d352470aa64cd9f0eb7ad19925
Bratrindersteak <2386140382@qq.com>
2026-08-28 22:03:30 +0800

    提交

--- src/utils/three/initSphere.ts
+++ src/utils/three/initSphere.ts
@@ -4,7 +4,7 @@ import { CARD_WIDTH, CARD_HEIGHT } from '@/config/constants.ts';
 
 import type { Vector3, Object3D } from 'three';
 
-function initSphere(index: number, total: number, vector: Vector3, targets: Object3D[]  ) {
+function initSphere(index: number, total: number, vector: Vector3, targets: Object3D[]) {
   // 1. 设置基础基础卡片间距系数（可根据卡片的真实宽高适当微调，比如 100~150）
   const baseSpacing =  Math.ceil(Math.sqrt((CARD_WIDTH ** 2) + (CARD_HEIGHT ** 2)) * 1.25 / Math.sqrt(4 * Math.PI));
   // 2. 动态计算球体半径：基础半径 + 根据总数开平方动态扩展
